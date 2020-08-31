0c252d4a91da651edfaa0713e65182e45bf709d6
Marielle de Jong <jong.de.me@gmail.com>
2020-08-31 14:55:06 +0200

    changed meeting model name to booking name

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Meeting < ApplicationRecord
   belongs_to :user
   has_one :address, as: :addressable
   has_one :skill
+  has_many :reviews
 
   validates :date, presence: true
   validates :status, presence: true
