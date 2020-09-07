852483160701737e3856afd2726cd3d2356a071f
Tommy Wentling <tommy.wentling@gmail.com>
2020-09-07 11:58:26 +0200

    Add has_many :reviews to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :bookings
   has_many :skills, dependent: :destroy
+  has_many :reviews
   accepts_nested_attributes_for :skills
   has_one_attached :photo
   accepts_nested_attributes_for :address
