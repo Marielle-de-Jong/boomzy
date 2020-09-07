9315bdca0faafdfb3b7f83aea1be73365046c51f
Tommy Wentling <tommy.wentling@gmail.com>
2020-09-07 10:32:44 +0200

    Specify url in simple_form to resolve path issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   def new
-    @booking = Booking.find(params[:booking_id])
+    # @booking = Booking.find(params[:booking_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    booking = Booking.find(params[:booking_id])
+    # booking = Booking.find(params[:booking_id])
     @review.booking = booking
     if @review.save
       redirect_to booking_path(booking)
@@ -23,4 +23,3 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:content, :rating)
   end
 end
-
