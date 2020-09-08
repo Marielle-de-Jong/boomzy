62119f1ffecf4d981af0e4e74af5bb1a6385133e
Alex Botwinick <alexbotwinick@gmail.com>
2020-09-08 10:24:31 +0200

    created susan, need square photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -146,182 +146,244 @@ booking = Booking.create(
 puts "----------------------------------------"
 puts "[LOG] BRIAN Completed ..."
 
-
-
-
-
-
-
-
-puts "[LOG] STARTING SEED GENERATION..."
-
-
-# --------------------
-# CREATE USERS - ADMIN
-# --------------------
 puts "----------------------------------------"
-puts "[LOG] creating ADMIN users..."
-a = 0
-ADMINS.each do |admin|
-  user = User.new
-  user.first_name = admin[0]
-  user.last_name = "Admin"
-  user.email = admin[1]
-  user.password = 'admin1'
-  user.password_confirmation = 'admin1'
-  user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis."
-  user.motivation = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis"
-  file_admin = URI.open("https://source.unsplash.com/900x900/?headshot")
-  user.photo.attach(io: file_admin, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
-  user.save!
-  a += 1
-end
-puts "[LOG] #{a} ADMINS created"
+puts "[LOG] creating SUSAN..."
 
+user = User.new
+user.first_name = "Susan"
+user.last_name = "Feldmen"
+# Email should be first_name@boomzy.me
+user.email = "susan@boomzy.me"
+# Password should be test123
+user.password = 'test123'
+user.password_confirmation = 'test123'
+user.bio = "I have been a photographer for over 40 years and I am always looking for new subjects. I moved to Amsterdam last year with my partner and am still looking to build a community here."
+user.motivation = "Photography and art are so important in difficult times like these. I hope to share the knowledge I've gained with people eager to learn!"
+file = URI.open("https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80")
+user.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
+user.save!
 
-# -------------------
-# CREATE USERS - TEST
-# -------------------
 puts "----------------------------------------"
-puts "[LOG] creating TEST users..."
-i = 0
-10.times do
-  user = User.new
-  user.first_name = Faker::Name.first_name
-  user.last_name = Faker::Name.last_name
-  user.email = Faker::Internet.email
-  user.password = 'test123'
-  user.password_confirmation = 'test123'
-  user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis. Nam sed iaculis purus. Praesent tincidunt congue ex, ut congue lacus rutrum faucibus. Nullam posuere urna eget enim rhoncus imperdiet. Aenean ut blandit nulla. Aenean dapibus non lectus et gravida. Curabitur at urna vestibulum, mattis risus vitae, consectetur elit. Morbi fringilla neque nec libero dapibus, non dapibus dui dignissim."
-  user.motivation = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis. Nam sed iaculis purus. Praesent tincidunt congue ex, ut congue lacus rutrum faucibus."
-  file = URI.open("https://source.unsplash.com/900x900/?headshot")
-  user.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
-  user.save!
-  i += 1
-end
-
-puts "[LOG] #{i} USERS created"
-puts "----------------------------------------"
-puts "[LOG] #{User.count} TOTAL users created"
+puts "[LOG] creating SUSAN's address..."
 
 
-# ----------------
-# CREATE ADDRESSES
-# ----------------
-puts "----------------------------------------"
-puts "[LOG] creating ADDRESSES..."
-User.all.each_with_index do |user, index|
-  location = STREETS_ZIPS.sample
-
-  Address.create(
+Address.create(
     city: "Amsterdam",
-    address_line_1: location[0],
-    postcode: location[1],
-    addressable: user
+    address_line_1: "Willem Passtoorsstraat 104",
+    postcode: "1073HX",
+    addressable: user,
     )
-# Address.create!(address_line_1: "Elandsgracht 86", postcode: "1016TZ", city: "Amsterdam", addressable_type: "User", addressable_id: User.first.id)
-
-end
-puts "[LOG] #{Address.count} ADDRESSES created"
-
-
-
-# -------------
-# CREATE SKILLS
-# -------------
 
 puts "----------------------------------------"
-puts "[LOG] creating SKILLS..."
-
-User.all.each do |user|
-  skill = Skill.create!(
-    name: Faker::Job.title,
-    category: CATEGORIES.sample,
-    user_id: user.id
-    )
-end
-puts "[LOG] #{Skill.count} SKILLS created"
+puts "[LOG] creating SUSAN's first skill, listings and bookings..."
 
 
-# ---------------
-# CREATE LISTINGS
-# ---------------
-puts "----------------------------------------"
-puts "[LOG] creating LISTINGS..."
-
-User.all.each do |user|
-  listing = Listing.create!(
-    title: Faker::Book.title,
+skill = Skill.create!(
+    name: "Portrait Photography",
+    category: "Photography",
     user_id: user.id,
-    skill_id: Skill.all.sample.id,
-    skill_level: LEVELS.sample,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
     )
-end
-puts "[LOG] #{Listing.count} LISTINGS created"
 
-# ------------------------
-# CREATE LISTING ADDRESSES
-# ------------------------
-puts "----------------------------------------"
-puts "[LOG] creating LISTING ADDRESSES..."
-Listing.all.each_with_index do |listing, index|
-  location = MEETING_SZ.sample
-  Address.create(
+listing = Listing.create!(
+    title: "Taking portraits with Susan",
+    user_id: user.id,
+    skill_id: skill.id,
+    skill_level: "Master",
+    description: "I have been a professional photographer for years, let me teach you about lighting and field of depth, "
+    )
+
+Address.create(
     city: "Amsterdam",
-    address_line_1: location[0],
-    postcode: location[1],
+    address_line_1: "Willem Passtoorsstraat 104",
+    postcode: "1073HX",
     addressable: listing
     )
-# Address.create!(address_line_1: "Elandsgracht 86", postcode: "1016TZ", city: "Amsterdam", addressable_type: "User", addressable_id: User.first.id)
 
-end
-puts "[LOG] #{Address.count} ADDRESSES created"
-
-# ---------------
-# CREATE BOOKINGS
-# ---------------
-puts "----------------------------------------"
-puts "[LOG] creating BOOKINGS..."
-
-Listing.all.each do |listing|
-  booking = Booking.create(
+booking = Booking.create(
     date: Faker::Date.between(from: '2020-09-23', to: '2020-09-25'),
     user_id: User.all.sample.id,
     status: STATUS.sample,
     listing_id: listing.id,
     )
-end
-puts "[LOG] #{Booking.count} BOOKINGS created"
 
-# ---------------
-# CREATE CHATROOMS
-# ---------------
 puts "----------------------------------------"
-puts "[LOG] creating CHATROOMS..."
+puts "[LOG] SUSAN Completed ..."
 
-Booking.all.each do |booking|
-  chatroom = Chatroom.new
-  chatroom.booking = booking
-  chatroom.save
-end
-puts "[LOG] #{Chatroom.count} CHATROOMS created"
 
-# ---------------
-# CREATE REVIEWS
-# ---------------
-puts "----------------------------------------"
-puts "[LOG] creating REVIEWS..."
 
-Booking.all.each do |booking|
-  review = Review.create!(
-    content: "My meeting was so great!",
-    rating: rand(1..5),
-    user_id: User.all.sample.id
-    )
-end
 
-puts "[LOG] #{Review.count} REVIEWS created"
+
+
+
+# puts "[LOG] STARTING SEED GENERATION..."
+
+
+# # --------------------
+# # CREATE USERS - ADMIN
+# # --------------------
+# puts "----------------------------------------"
+# puts "[LOG] creating ADMIN users..."
+# a = 0
+# ADMINS.each do |admin|
+#   user = User.new
+#   user.first_name = admin[0]
+#   user.last_name = "Admin"
+#   user.email = admin[1]
+#   user.password = 'admin1'
+#   user.password_confirmation = 'admin1'
+#   user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis."
+#   user.motivation = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis"
+#   file_admin = URI.open("https://source.unsplash.com/900x900/?headshot")
+#   user.photo.attach(io: file_admin, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
+#   user.save!
+#   a += 1
+# end
+# puts "[LOG] #{a} ADMINS created"
+
+
+# # -------------------
+# # CREATE USERS - TEST
+# # -------------------
+# puts "----------------------------------------"
+# puts "[LOG] creating TEST users..."
+# i = 0
+# 10.times do
+#   user = User.new
+#   user.first_name = Faker::Name.first_name
+#   user.last_name = Faker::Name.last_name
+#   user.email = Faker::Internet.email
+#   user.password = 'test123'
+#   user.password_confirmation = 'test123'
+#   user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis. Nam sed iaculis purus. Praesent tincidunt congue ex, ut congue lacus rutrum faucibus. Nullam posuere urna eget enim rhoncus imperdiet. Aenean ut blandit nulla. Aenean dapibus non lectus et gravida. Curabitur at urna vestibulum, mattis risus vitae, consectetur elit. Morbi fringilla neque nec libero dapibus, non dapibus dui dignissim."
+#   user.motivation = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo imperdiet turpis, eget imperdiet dolor facilisis quis. Nam sed iaculis purus. Praesent tincidunt congue ex, ut congue lacus rutrum faucibus."
+#   file = URI.open("https://source.unsplash.com/900x900/?headshot")
+#   user.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
+#   user.save!
+#   i += 1
+# end
+
+# puts "[LOG] #{i} USERS created"
+# puts "----------------------------------------"
+# puts "[LOG] #{User.count} TOTAL users created"
+
+
+# # ----------------
+# # CREATE ADDRESSES
+# # ----------------
+# puts "----------------------------------------"
+# puts "[LOG] creating ADDRESSES..."
+# User.all.each_with_index do |user, index|
+#   location = STREETS_ZIPS.sample
+
+#   Address.create(
+#     city: "Amsterdam",
+#     address_line_1: location[0],
+#     postcode: location[1],
+#     addressable: user
+#     )
+# # Address.create!(address_line_1: "Elandsgracht 86", postcode: "1016TZ", city: "Amsterdam", addressable_type: "User", addressable_id: User.first.id)
+
+# end
+# puts "[LOG] #{Address.count} ADDRESSES created"
+
+
+
+# # -------------
+# # CREATE SKILLS
+# # -------------
+
+# puts "----------------------------------------"
+# puts "[LOG] creating SKILLS..."
+
+# User.all.each do |user|
+#   skill = Skill.create!(
+#     name: Faker::Job.title,
+#     category: CATEGORIES.sample,
+#     user_id: user.id
+#     )
+# end
+# puts "[LOG] #{Skill.count} SKILLS created"
+
+
+# # ---------------
+# # CREATE LISTINGS
+# # ---------------
+# puts "----------------------------------------"
+# puts "[LOG] creating LISTINGS..."
+
+# User.all.each do |user|
+#   listing = Listing.create!(
+#     title: Faker::Book.title,
+#     user_id: user.id,
+#     skill_id: Skill.all.sample.id,
+#     skill_level: LEVELS.sample,
+#     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+#     )
+# end
+# puts "[LOG] #{Listing.count} LISTINGS created"
+
+# # ------------------------
+# # CREATE LISTING ADDRESSES
+# # ------------------------
+# puts "----------------------------------------"
+# puts "[LOG] creating LISTING ADDRESSES..."
+# Listing.all.each_with_index do |listing, index|
+#   location = MEETING_SZ.sample
+#   Address.create(
+#     city: "Amsterdam",
+#     address_line_1: location[0],
+#     postcode: location[1],
+#     addressable: listing
+#     )
+# # Address.create!(address_line_1: "Elandsgracht 86", postcode: "1016TZ", city: "Amsterdam", addressable_type: "User", addressable_id: User.first.id)
+
+# end
+# puts "[LOG] #{Address.count} ADDRESSES created"
+
+# # ---------------
+# # CREATE BOOKINGS
+# # ---------------
+# puts "----------------------------------------"
+# puts "[LOG] creating BOOKINGS..."
+
+# Listing.all.each do |listing|
+#   booking = Booking.create(
+#     date: Faker::Date.between(from: '2020-09-23', to: '2020-09-25'),
+#     user_id: User.all.sample.id,
+#     status: STATUS.sample,
+#     listing_id: listing.id,
+#     )
+# end
+# puts "[LOG] #{Booking.count} BOOKINGS created"
+
+# # ---------------
+# # CREATE CHATROOMS
+# # ---------------
+# puts "----------------------------------------"
+# puts "[LOG] creating CHATROOMS..."
+
+# Booking.all.each do |booking|
+#   chatroom = Chatroom.new
+#   chatroom.booking = booking
+#   chatroom.save
+# end
+# puts "[LOG] #{Chatroom.count} CHATROOMS created"
+
+# # ---------------
+# # CREATE REVIEWS
+# # ---------------
+# puts "----------------------------------------"
+# puts "[LOG] creating REVIEWS..."
+
+# Booking.all.each do |booking|
+#   review = Review.create!(
+#     content: "My meeting was so great!",
+#     rating: rand(1..5),
+#     user_id: User.all.sample.id
+#     )
+# end
+
+# puts "[LOG] #{Review.count} REVIEWS created"
 
 puts "----------------------------------------"
 puts "[LOG] End time: #{Time.now}"
